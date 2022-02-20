c4cff7939b1461a9f7f377e8f60cc8eb7447908c
Joe Baker <joesbaker@gmail.com>
2022-02-20 16:15:58 +0000

    Scroll a random testimonial into view on Soul Songs page

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -14,4 +14,14 @@ $(document).ready(function () {
       $('button.site-header__menu').attr('aria-expanded', 'true');
     }
   });
+
+  if ($('.testimonials--container')[0] && $('.testimonials--container').data('testimonial-count') > 1) {
+    var count = $('.testimonials--container').data('testimonial-count');
+    var rand = Math.floor((Math.random() * count) + 1);
+    var name = '.testimonial--' + rand;
+    if (rand > 1) {
+      var testimonial = $(name);
+      testimonial[0].scrollIntoView();
+    }
+  }
 });
